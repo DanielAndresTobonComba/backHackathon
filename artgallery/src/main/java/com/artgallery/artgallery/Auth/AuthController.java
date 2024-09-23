6c35a3ad25cd24fcf82e1cac2a1f3c883507718e
Daniel Andres Tobon Comba <toboncombadanielandres@gmail.com>
2024-09-23 12:31:27 -0500

    cross 1

--- artgallery/src/main/java/com/artgallery/artgallery/Auth/AuthController.java
+++ artgallery/src/main/java/com/artgallery/artgallery/Auth/AuthController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import lombok.RequiredArgsConstructor;
 
-@CrossOrigin(origins = "*")
+@CrossOrigin(value = "*")
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
