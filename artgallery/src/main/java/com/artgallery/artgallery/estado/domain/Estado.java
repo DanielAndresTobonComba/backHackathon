63f7bf4f3a70d73338f0abbf36d71ebadba543c0
christiancelis <73555661+christiancelis@users.noreply.github.com>
2024-09-24 15:43:35 -0500

    aa

--- artgallery/src/main/java/com/artgallery/artgallery/estado/domain/Estado.java
+++ artgallery/src/main/java/com/artgallery/artgallery/estado/domain/Estado.java
@@ -1,13 +1,11 @@
 package com.artgallery.artgallery.estado.domain;
 
-import com.artgallery.artgallery.proyecto.domain.Proyecto;
+
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
